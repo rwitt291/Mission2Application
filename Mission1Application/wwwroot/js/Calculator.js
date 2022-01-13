2dcc4816a3b37fb9b9524bc7880e7db2936f97f3
rwitt291 <90530778+rwitt291@users.noreply.github.com>
2022-01-12 18:33:46 -0700

    Add project files.

---
+++ Mission1Application/wwwroot/js/Calculator.js
@@ -0,0 +1,27 @@
+﻿function addPercentInputs() {
+    var assignments = parseInt$("#intAssignments").val();
+    var group = parseInt$("#intGroup").val();
+    var quizzes = parseInt$("#intQuizzes").val();
+    var exams = parseInt$("#intExams").val();
+    var intex = parseInt$("#intINTEX").val();
+}
+
+var CalcGrade = (assignments + group + quizzes + exams + intex);
+let letter = "";
+
+if (CalcGrade < 60) {
+    letter = "F";
+} else if (CalcGrade < 70) {
+    letter = "D";
+} else if (CalcGrade < 80) {
+    letter = "C";
+} else if (CalcGrade < 90) {
+    letter = "B";
+} else {
+    letter = "A";
+}
+
+
+$("#butnSend").click(function () {
+    alert("Calculated Grade: " + CalcGrade + ", Letter Grade: " + letter);
+})
