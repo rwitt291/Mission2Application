eb9c37f931f198bd366490ac248abed5a3d65664
rwitt291 <90530778+rwitt291@users.noreply.github.com>
2022-01-19 20:58:30 -0700

    Mission 3

---
+++ Mission1Application/Controllers/AboutController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission1Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission1Application.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("GradeCalculator")]
+        public IActionResult GradeCalculator ()
+        {
+            return View();
+        }
+
+        [HttpPost("GradeCalculator")]
+        public IActionResult GradeCalculator (CalcModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission1Application/Models/CalcModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission1Application.Models
+{
+    public class CalcModel
+    {
+
+        [Range(0, 100, ErrorMessage = "Input must be between 0 and 100")]
+        public decimal assignments { get; set; }
+        [Range(0, 100, ErrorMessage = "Input must be between 0 and 100")]
+        public decimal groupProject { get; set; }
+        [Range(0, 100, ErrorMessage = "Input must be between 0 and 100")]
+        public decimal quizzes { get; set; }
+        [Range(0, 100, ErrorMessage = "Input must be between 0 and 100")]
+        public decimal exams { get; set; }
+        [Range(0, 100, ErrorMessage = "Input must be between 0 and 100")]
+        public int intex { get; set; }
+
+    }
+}
